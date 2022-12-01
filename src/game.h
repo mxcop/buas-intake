91916e1a4b60c1f575760b2eb89f068ea831e93a
Max Coppen <44031065+mxcop@users.noreply.github.com>
2022-12-01 20:43:28 +0100

    refactor: cleanup game.h

--- src/game.h
+++ src/game.h
@@ -13,57 +13,28 @@ using std::shared_ptr;
 using std::unique_ptr;
 
 namespace Tmpl8 {
-
+	/* constants */
 	constexpr int ScreenScalingFactor = 4;
-
 	constexpr int TilemapWidth = 16;
 	constexpr int TilemapHeight = 16;
-
 	constexpr int TileSize = 8;
 
 	class Surface;
 
-	//class Game
-	//{
-	//	public:
-	//		static unique_ptr<Tilemap> tilemap;
-	//		void SetTarget( Surface* surface ) { screen = surface; }
-	//		void Init( SDL_Window* win );
-	//		void Shutdown();
-	//		void Tick( float deltaTime );
-	//		void MouseUp( int button ) { /* implement if you want to detect mouse button presses */ }
-	//		void MouseDown( int button ) { /* implement if you want to detect mouse button presses */ }
-	//		void MouseMove(int dx, int dy);
-	//		void KeyUp( int key ) { /* implement if you want to handle keys */ }
-	//		void KeyDown(int key);
-	//	private:
-	//		/* windowing */
-	//		Surface* screen = nullptr;
-	//		SDL_Window* window = nullptr;
-
-	//		/* globals */
-	//		unsigned long frame = 0u;
-	//		int2 mouse = int2();
-	//		float2 f_mouse = float2();
-
-	//		/* objects */
-	//		unique_ptr<Player> player = nullptr;
-	//		shared_ptr<EnemyArena> enemies = nullptr;
-	//};
-
 	class Game
 	{
 	public:
+		// Don't allow creation of instances outside the class:
 		Game(Game const&) = delete;
 		void operator=(Game const&) = delete;
 
-		// C++ singleton pattern : https://stackoverflow.com/questions/1008019/c-singleton-design-pattern
-		static std::shared_ptr<Game> instance()
-		{
+		// C++ singleton pattern : <https://stackoverflow.com/questions/1008019/c-singleton-design-pattern>
+		static std::shared_ptr<Game> instance() {
 			static std::shared_ptr<Game> s{ new Game };
 			return s;
 		}
 
+		/* functions */
 		void SetTarget(Surface* surface) { screen = surface; }
 		void Init(SDL_Window* win);
 		void Shutdown();
@@ -74,6 +45,7 @@ namespace Tmpl8 {
 		void KeyUp(int key) { /* implement if you want to handle keys */ }
 		void KeyDown(int key);
 
+		/* objects */
 		shared_ptr<Tilemap> tilemap = nullptr;
 
 	private:
